56136f39845175eb534e90e730d32d22f86fb133
Pieter Dewachter <pieter.dewachter@outlook.be>
2022-09-01 14:14:24 +0200

    Automatically bypass cookiewall

--- service.js
+++ service.js
@@ -1,9 +1,13 @@
 chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
     if (request.contentScriptQuery == "queryToernooi") {
-        let toernooiUrl = "https://www.toernooi.nl/player-profile/" + request.playerUuid + "/Rating"
-        fetch(toernooiUrl)
-            .then(response => response.text())
-            .then(data => sendResponse(data))
+        let cookieUrl = "https://www.toernooi.nl/cookiewall/Save"
+        fetch(cookieUrl, { method: "POST" })
+            .then(_ => {
+                let toernooiUrl = "https://www.toernooi.nl/player-profile/" + request.playerUuid + "/Rating"
+                fetch(toernooiUrl)
+                    .then(response => response.text())
+                    .then(data => sendResponse(data))
+            })
         return true
       }
 } )
